cac1cfea638a009a37ba04cd2d5f7510a030deed
Philippe Schommers <philippe@schommers.be>
2022-05-04 16:53:47 +0200

    feat: add SimpleDeal and MintableERC20

---
+++ src/MintableERC20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Auth, Authority} from "solmate/auth/Auth.sol";
+
+contract MintableERC20 is ERC20, Auth {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    )
+        ERC20(_name, _symbol, _decimals)
+        Auth(msg.sender, Authority(address(0)))
+    {}
+
+    function mint(address to, uint256 amount) external requiresAuth {
+        _mint(to, amount);
+    }
+}
