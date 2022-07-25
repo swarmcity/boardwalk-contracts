07c07dc4233f89a14ab44418b1eb708ce3cfed38
Philippe Schommers <philippe@schommers.be>
2022-07-25 12:39:12 +0200

    test: add basic hashtag tests

--- test/Hashtag.t.sol
+++ test/Hashtag.t.sol
@@ -9,6 +9,11 @@ import {Hashtag} from 'src/Hashtag.sol';
 import {MintableERC20} from 'src/MintableERC20.sol';
 
 contract HashtagTest is Test {
+	// Events
+	event SetPayoutAddress(address payoutAddress);
+	event SetMetadataHash(string metadataHash);
+	event SetFee(uint256 fee);
+
 	// Constants
 	MintableERC20 ZERO_MINTABLE = MintableERC20(address(0));
 
@@ -20,26 +25,83 @@ contract HashtagTest is Test {
 	MintableERC20 seekerRep;
 
 	// Accounts
-	address seeker = address(0x1);
-	address provider = address(0x2);
-	address maintainer = address(0x3);
+	address seeker = address(1);
+	address provider = address(2);
+	address maintainer = address(3);
 
 	function setUp() public {
+		// Create contracts
 		vm.startPrank(maintainer);
 		token = new MockERC20('Swarm City', 'SWT', 18);
-		hashtag = new Hashtag(address(token), 'Marketplace', 25e16, 'SomeHash');
+		hashtag = new Hashtag(address(token), 'Marketplace', 50e16, 'SomeHash');
 
 		providerRep = hashtag.providerRep();
 		seekerRep = hashtag.seekerRep();
+
+		// Mint tokens
+		token.mint(seeker, 100e18);
+		vm.stopPrank();
 	}
 
 	function testMetadata() public {
 		assertEq(hashtag.name(), 'Marketplace');
-		assertEq(hashtag.fee(), 25e16);
+		assertEq(hashtag.fee(), 50e16);
 		assertEq(address(hashtag.token()), address(token));
 		assertEq(hashtag.payoutAddress(), maintainer);
 		assertEq(hashtag.metadataHash(), 'SomeHash');
 		assertTrue(providerRep != ZERO_MINTABLE);
 		assertTrue(seekerRep != ZERO_MINTABLE);
+	}
+
+	function testCanChangePayoutAddress() public {
+		address user = address(99);
+
+		vm.expectEmit(true, true, false, false);
+		emit SetPayoutAddress(user);
+		vm.prank(maintainer);
+		hashtag.setPayoutAddress(user);
+
+		assertEq(hashtag.payoutAddress(), user);
+	}
+
+	function testCanChangeMetadataHash() public {
+		string memory newHash = 'NewMetadataHash';
+
+		vm.expectEmit(true, true, false, false);
+		emit SetMetadataHash(newHash);
+		vm.prank(maintainer);
+		hashtag.setMetadataHash(newHash);
+
+		assertEq(hashtag.metadataHash(), newHash);
+	}
+
+	function testCanChangeFee() public {
+		uint256 fee = 123456;
+
+		vm.expectEmit(true, true, false, false);
+		emit SetFee(fee);
+		vm.prank(maintainer);
+		hashtag.setFee(fee);
+
+		assertEq(hashtag.fee(), fee);
+	}
+
+	function testCanCreateItem() public {
+		vm.startPrank(seeker);
+		token.approve(address(hashtag), 10e18 + 25e16);
+		hashtag.newItem('ItemHash', 10e18, 'ItemMetadata');
+
+		assertEq(token.balanceOf(seeker), 100e18 - 10e18 - 25e16);
+		assertEq(token.balanceOf(maintainer), 25e16);
+		assertEq(token.balanceOf(address(hashtag)), 10e18);
+	}
+
+	function testCannotCreateSameItem() public {
+		vm.startPrank(seeker);
+		token.approve(address(hashtag), 10e18 + 2 * 25e16);
+		hashtag.newItem('ItemHash', 10e18, 'ItemMetadata');
+
+		vm.expectRevert('ITEM_ALREADY_EXISTS');
+		hashtag.newItem('ItemHash', 15e18, 'OtherMetadata');
 	}
 }
